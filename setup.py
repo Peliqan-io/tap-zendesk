e2f02b24d8fff0b918b9489f701cea7b3898237e
Mohammed-Reda Daoudi <37927504+mredaoudi@users.noreply.github.com>
2022-12-05 13:24:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-zendesk',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_zendesk'],
       install_requires=[
-          'singer-python==5.12.2',
+          'singer-python @ git+https://github.com/peliqan-io/singer-python@master',
           'zenpy==2.0.24',
           'backoff==1.8.0',
           'requests==2.25.1',
